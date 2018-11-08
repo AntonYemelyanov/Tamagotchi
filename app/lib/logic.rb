61bdedc04c1d10452dc56ec520b46bb534be50b9
MaksGostroushko <makshostroyshko@gmail.com>
2018-11-08 16:32:11 +0200

    This save all logic of programme

---
+++ app/lib/logic.rb
@@ -0,0 +1,15 @@
+module Logic
+  def self.change_params(req, name)
+
+    Rack::Response.new do |response|
+      response.set_cookie(name, req.cookies["#{name}"].to_i + 10)
+
+      need = ($NEEDS - [name]).sample
+
+      response.set_cookie(need, req.cookies["#{need}"].to_i - rand(5..15))
+      response.set_cookie(need, 0) if req.cookies["#{need}"].to_i < 0
+
+      response.redirect('/start')
+    end
+  end
+end
---
+++ app/pet.rb
@@ -0,0 +1,64 @@
+require "erb"
+require './app/lib/logic'
+
+class Pet
+  include Logic
+
+  def self.call(env)
+    new(env).response.finish
+  end
+
+  def initialize(env)
+    @req    = Rack::Request.new(env)
+    @food   = 100
+    @health = 100
+    @sleep  = 100
+    @happy  = 100
+    $NEEDS  = %w[food health sleep happy]
+  end
+
+  def response
+    case @req.path
+    when '/'
+      Rack::Response.new(render("form.html.erb"))
+    when '/initialize'
+      Rack::Response.new do |response|
+        response.set_cookie('food', @food)
+        response.set_cookie('sleep', @sleep)
+        response.set_cookie('health', @health)
+        response.set_cookie('happy', @happy)
+        response.set_cookie('name', @req.params['name'])
+        response.redirect('/start')
+      end
+    when '/start'
+      $NEEDS.each do |need|
+        if get("#{need}") <= 0
+          return Rack::Response.new('Game Over', 404)
+        else
+          return Rack::Response.new(render("index.html.erb"))
+        end
+      end
+    when '/change'
+      return Logic.change_params(@req, 'food') if @req.params['food']
+      return Logic.change_params(@req, 'health') if @req.params['health']
+      return Logic.change_params(@req, 'sleep') if @req.params['sleep']
+      return Logic.change_params(@req, 'happy') if @req.params['happy']
+    else
+      Rack::Response.new('Not Found', 404)
+    end
+  end
+
+  def render(template)
+    path = File.expand_path("../views/#{template}", __FILE__)
+    ERB.new(File.read(path)).result(binding)
+  end
+
+  def name
+    name = @req.cookies['name'].delete(' ')
+    name.empty? ? 'Pet' : @req.cookies['name']
+  end
+
+  def get(attr)
+    @req.cookies["#{attr}"].to_i
+  end
+end
